ca81de2f3bcf4769a12c24f32a19fd347b792cfb
davidecrs <corso.davide.92@gmail.com>
2020-12-26 17:15:45 +0100

    Added SpatialDE.run and .model_search functions

--- R/spatialDE.R
+++ R/spatialDE.R
@@ -14,3 +14,30 @@ list_spatialDE_methods <- function() {
     })
     names
 }
+
+#' @note the function 'spatialDE$run' run with tqdm python library, which
+#' allow to view a progress bar and it's not suitable for R.
+#' TODO: hide progress bar.
+#' 
+#' @importFrom reticulate import
+spatialDE_run <- function(coordinates, res) {
+    spatialDE <- import("SpatialDE")
+    
+    X <- r_to_py(coordinates)
+    res_py <- r_to_py(res)
+    
+    results <- spatialDE$run(X, res_py)
+    return(results)
+}
+
+#' @importFrom reticulate import
+spatialDE_model_search <- function(coordinates, res, de_results) {
+    spatialDE <- import("SpatialDE")
+    
+    X <- r_to_py(coordinates)
+    res_py <- r_to_py(res)
+    de_results_py <- r_to_py(de_results)
+    
+    ms_results <- spatialDE$model_search(X, res_py, de_results_py)
+    return(ms_results)
+}
