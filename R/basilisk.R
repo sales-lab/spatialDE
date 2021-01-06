6e7220c28ef3601a41722bbf2d95bf7106b9543b
Milan Malfait <milan.malfait94@gmail.com>
2021-01-06 20:11:16 +0100

    Add NaiveDE as basilisk dependency

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,5 +1,5 @@
 #' @importFrom basilisk BasiliskEnvironment
 spatialDE_env <- BasiliskEnvironment(
     envname = "env", pkgname = "spatialDE",
-    packages = character(0), pip = "SpatialDE==1.1.3"
+    packages = character(0), pip = c("SpatialDE==1.1.3", "NaiveDE==1.2.0")
 )
