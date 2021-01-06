f8ae3fa5b42428f8df34a86935926a4e93fa6794
Milan Malfait <milan.malfait94@gmail.com>
2021-01-06 19:41:19 +0100

    Use dot notation for non-exported functions

--- R/naiveDE.R
+++ R/naiveDE.R
@@ -1,40 +1,40 @@
-#' Wrapper for SpatialDE.run python function
-#' 
+#' Wrapper for NaiveDE.stabilize python function
+#'
 #' @description Wrapped functions from NaiveDE python package
 #' [NaiveDE](https://github.com/Teichlab/NaiveDE)
-#' 
+#'
 #' @param df dataframe with the expression values. Columns are genes, and Rows
 #' are samples
 #'
 #' @importFrom reticulate import r_to_py
-naiveDE_stabilize <- function(df) {
-  naiveDE <- import("NaiveDE")
-  
-  dft <- as.data.frame(t(df))
-  dft_py <- r_to_py(dft)
-  
-  stabilized <- naiveDE$stabilize(dft_py)
-  
-  tstabilized <- as.data.frame(t(stabilized))
-  return(tstabilized)
+.naiveDE_stabilize <- function(df) {
+    naiveDE <- import("NaiveDE")
+
+    dft <- as.data.frame(t(df))
+    dft_py <- r_to_py(dft)
+
+    stabilized <- naiveDE$stabilize(dft_py)
+
+    tstabilized <- as.data.frame(t(stabilized))
+    return(tstabilized)
 }
 
 #' Wrapper for NaiveDE.regress_out python function
-#' 
-#' @param sample_info dataframe with samples as rows, 
-#' 'x', 'y' coordinates and  total raw counts as columns. 
+#'
+#' @param sample_info dataframe with samples as rows,
+#' 'x', 'y' coordinates and  total raw counts as columns.
 #'
 #' @param dfm dataframe resulting from naiveDE_stabilize
 #'
 #' @importFrom reticulate import r_to_py
-naiveDE_regress_out <- function(sample_info, dfm) {
-  naiveDE <- import("NaiveDE")
-  
-  sample_info_py <- r_to_py(sample_info)
-  tdfm_py <- r_to_py(as.data.frame(t(dfm)))
-  
-  res <- naiveDE$regress_out(sample_info_py, tdfm_py, 'np.log(total_counts)')
-  
-  tres <- as.data.frame(t(res))
-  return(tres)
+.naiveDE_regress_out <- function(sample_info, dfm) {
+    naiveDE <- import("NaiveDE")
+
+    sample_info_py <- r_to_py(sample_info)
+    tdfm_py <- r_to_py(as.data.frame(t(dfm)))
+
+    res <- naiveDE$regress_out(sample_info_py, tdfm_py, "np.log(total_counts)")
+
+    tres <- as.data.frame(t(res))
+    return(tres)
 }
--- R/spatialDE.R
+++ R/spatialDE.R
@@ -16,45 +16,45 @@ list_spatialDE_methods <- function() {
 }
 
 #' Wrapper for SpatialDE.run python function
-#' 
+#'
 #' @param coordinates dataframe with sample coordinates. Each row is a sample,
-#' the columns with coordinates must be named 'x' and 'y'. 
+#' the columns with coordinates must be named 'x' and 'y'.
 #'
 #' @param res dataframe resulting from naiveDE_regress_out function
 #'
 #' @note both functions of SpatialDE run with tqdm python library, which
 #' allow to view a progress bar and it's not suitable for R.
 #' TODO: hide prints of progress bar
-#' 
+#'
 #' @importFrom reticulate import r_to_py
-spatialDE_run <- function(coordinates, res) {
+.spatialDE_run <- function(coordinates, res) {
     spatialDE <- import("SpatialDE")
-    
+
     X <- r_to_py(coordinates)
     res_py <- r_to_py(res)
-    
+
     results <- spatialDE$run(X, res_py)
     return(results)
 }
 
 #' Wrapper for SpatialDE.model_search python function
-#' 
+#'
 #' @param coordinates dataframe with sample coordinates. Each row is a sample,
-#' the columns with coordinates must be named 'x' and 'y'. 
+#' the columns with coordinates must be named 'x' and 'y'.
 #'
 #' @param res dataframe resulting from naiveDE_regress_out function
-#' 
+#'
 #' @param de_results dataframe resulting from spatialDE_run filtered based on
 #' qvalue < threshold (e.g. qvalue < 0.05)
 #'
 #' @importFrom reticulate import r_to_py
-spatialDE_model_search <- function(coordinates, res, de_results) {
+.spatialDE_model_search <- function(coordinates, res, de_results) {
     spatialDE <- import("SpatialDE")
-    
+
     X <- r_to_py(coordinates)
     res_py <- r_to_py(res)
     de_results_py <- r_to_py(de_results)
-    
+
     ms_results <- spatialDE$model_search(X, res_py, de_results_py)
     return(ms_results)
 }
