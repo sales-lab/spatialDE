1a3affa4eb17552b0654263617bbaef02ea07e29
Milan Malfait <milan.malfait94@gmail.com>
2021-01-06 22:58:36 +0100

    NaiveDE requires 'patsy' module

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,5 +1,6 @@
 #' @importFrom basilisk BasiliskEnvironment
 spatialDE_env <- BasiliskEnvironment(
     envname = "env", pkgname = "spatialDE",
-    packages = character(0), pip = c("SpatialDE==1.1.3", "NaiveDE==1.2.0")
+    packages = character(0),
+    pip = c("SpatialDE==1.1.3", "NaiveDE==1.2.0", "patsy==0.5.1")
 )
