8ee8b7e54efdedd237c7522e48e57bb718bea5e4
Milan Malfait <milan.malfait94@gmail.com>
2021-01-09 19:32:10 +0100

    Remove `list_spatialDE_methods()` (example function)

--- R/spatialDE.R
+++ R/spatialDE.R
@@ -1,20 +1,3 @@
-#' List available methods from SpatialDE
-#'
-#' Shows the available methods from the
-#' [SpatialDE](https://github.com/Teichlab/SpatialDE) Python module.
-#'
-#' @export
-#'
-#' @examples
-#' list_spatialDE_methods()
-list_spatialDE_methods <- function() {
-    names <- basilisk::basiliskRun(env = spatialDE_env, fun = function() {
-        X <- reticulate::import("SpatialDE")
-        names(X)
-    })
-    names
-}
-
 #' Wrapper for SpatialDE.run python function
 #'
 #' @param coordinates `data.frame` with sample coordinates.
