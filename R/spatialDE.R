bc2bbd87095b5531d9dacadee9eb497d6fe0f338
davidecrs <corso.davide.92@gmail.com>
2021-01-21 15:59:23 +0100

    Added spatial_patterns function

--- R/spatialDE.R
+++ R/spatialDE.R
@@ -120,3 +120,84 @@ model_search <- function(coordinates, regressed_counts, de_results,
 
     spatialDE$model_search(X, res_py, de_results_py)
 }
+
+
+
+#' Group spatially variable genes into spatial patterns using automatic 
+#' expression histology (AEH)
+#'
+#' @param coordinates `data.frame` with sample coordinates.
+#' Each row is a sample, the columns with coordinates must be named 'x' and 'y'.
+#' 
+#' @param regressed_counts `data.frame` or `matrix` resulting from
+#' [regress_out()].
+#' 
+#' @param sres `data.frame` resulting from [run()] filtered
+#' based on `qvalue < threshold` (e.g. `qvalue < 0.05`)
+#' 
+#' @param C `integer` The number of spatial patterns
+#' 
+#' @param l `numeric`The charancteristic length scale of the clusters
+#' 
+#' @param verbose `bool` controlling the display of the progress messages.
+#'
+#' @return `list` of two dataframe (pattern_results, patterns):
+#' `pattern_results` dataframe with pattern membersehip information for each 
+#' gene.
+#' `patterns` the posterior mean underlying expression fro genes in given 
+#' spatial patterns.
+#' 
+#' @examples
+#' \dontrun{
+#' ncells <- 100
+#' ngenes <- 1000
+#' counts <- matrix(rpois(ncells * ngenes, lambda = 3),
+#'                  nrow = ngenes, ncol = ncells)
+#'
+#' ## Provide total counts for library size normalization and coordinates
+#' sample_info <- data.frame(total_counts = colSums(counts))
+#' coordinates <- data.frame(x = rnorm(ncells), y = rnorm(ncells))
+#'
+#' stabilized <- stabilize(counts)
+#' regressed <- regress_out(sample_info, stabilized)
+#'
+#' ## Run SpatialDE
+#' results <- run(coordinates, regressed)
+#' sres <- results[results$qval<0.05, ]
+#'
+#' ## Run Spatial_patterns
+#' sp <- spatial_patterns(coordinates, regressed, res, 5, 1.5)
+#' 
+#' sp$pattern_results
+#' sp$patterns
+#' }
+#' 
+#' @export
+spatial_patterns <- function(coordinates, regressed_counts, sres, C, l, 
+                             verbose=FALSE) {
+    out <- basilisk::basiliskRun(
+        env = spatialDE_env,
+        fun = .spatialDE_spatial_patterns,
+        coordinates = coordinates, regressed_counts = regressed_counts, 
+        sres = sres, C = C, l = l, verbose = verbose
+    )
+    out
+}
+
+
+#' @importFrom reticulate r_to_py
+.spatialDE_spatial_patterns <- function(coordinates, regressed_counts, sres, C, 
+                                        l, verbose) {
+    spatialDE <- .importPyModule(!verbose)
+    
+    X <- r_to_py(coordinates)
+    regr_out_py <- r_to_py(as.data.frame(t(regressed_counts)))
+    sres_py <- r_to_py(sres)
+    
+    spatterns <- spatialDE$spatial_patterns(X, regr_out_py, sres_py, 
+                                            as.integer(C), l)
+    return(list(
+        pattern_results = spatterns[[1]],
+        patterns = spatterns[[2]]
+    ))
+}
