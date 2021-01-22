7463c066a7c2991bab2a38dd6990a39da6d54f4e
Milan Malfait <milan.malfait94@gmail.com>
2021-01-22 10:08:47 +0100

    Add warning for stabilize() output and test

--- tests/testthat/test-spatialDE.R
+++ tests/testthat/test-spatialDE.R
@@ -1,7 +1,7 @@
 ncells <- 10
 ngenes <- 100
 
-## FIXME: `stabilize()` returns all NA values for some count inputs
+## Note: `stabilize()` returns all NA values for some count inputs
 ## Setting a seed to get the same counts for which stabilize() works for now
 set.seed(1)
 counts <- matrix(rpois(ncells * ngenes, lambda = 3),
@@ -21,4 +21,14 @@ test_that("Wrapper functions work", {
 
     ms_results <- model_search(coordinates, regressed, de_results)
     expect_false(all(is.na(ms_results)))
+})
+
+test_that("stabilize() warns about NA output", {
+    ## Setting a seed to get counts for which stabilize() doesn't work
+    set.seed(42)
+    counts <- matrix(rpois(ncells * ngenes, lambda = 3),
+                     nrow = ngenes, ncol = ncells)
+
+    expect_warning(stabilized <- stabilize(counts))
+    expect_true(all(is.na(stabilized)))
 })
