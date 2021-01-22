ba0ca8e2b97e56194825334ed87a7de83f6864dd
Milan Malfait <milan.malfait94@gmail.com>
2021-01-22 10:54:47 +0100

    Add more tests

--- tests/testthat/test-spatialDE.R
+++ tests/testthat/test-spatialDE.R
@@ -4,10 +4,10 @@ ngenes <- 100
 ## Note: `stabilize()` returns all NA values for some count inputs
 ## Setting a seed to get the same counts for which stabilize() works for now
 set.seed(1)
-counts <- matrix(rpois(ncells * ngenes, lambda = 3),
+counts <- matrix(stats::rpois(ncells * ngenes, lambda = 3),
                  nrow = ngenes, ncol = ncells)
 sample_info <- data.frame(total_counts = colSums(counts))
-coordinates <- data.frame(x = rnorm(ncells), y = rnorm(ncells))
+coordinates <- data.frame(x = stats::rnorm(ncells), y = stats::rnorm(ncells))
 
 test_that("Wrapper functions work", {
     stabilized <- stabilize(counts)
@@ -26,9 +26,33 @@ test_that("Wrapper functions work", {
 test_that("stabilize() warns about NA output", {
     ## Setting a seed to get counts for which stabilize() doesn't work
     set.seed(42)
-    counts <- matrix(rpois(ncells * ngenes, lambda = 3),
+    counts <- matrix(stats::rpois(ncells * ngenes, lambda = 3),
                      nrow = ngenes, ncol = ncells)
 
     expect_warning(stabilized <- stabilize(counts))
     expect_true(all(is.na(stabilized)))
+})
+
+test_that("run() returns correct output", {
+    out <- run(coordinates = coordinates, regressed_counts = counts)
+    expect_equal(nrow(out), nrow(counts))
+    expect_true(is.data.frame(out))
+
+    ## Check breaking errors (incompatible dimensions)
+    expect_error(run(coordinates = coordinates[1:3, ],
+                     regressed_counts = counts))
+})
+
+test_that("model_search() returns correct output", {
+    de_res <- run(coordinates, counts)
+    out <- model_search(coordinates = coordinates,
+                        regressed_counts = counts,
+                        de_results = de_res)
+    expect_equal(nrow(out), nrow(counts))
+    expect_true(is.data.frame(out))
+
+    ## Check breaking errors (incompatible dimensions)
+    expect_error(model_search(coordinates = coordinates[1:3, ],
+                              regressed_counts = counts,
+                              de_results = de_res))
 })
