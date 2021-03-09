d8aca21151985a993f2ee65c49728fbb3da6544e
Milan Malfait <milan.malfait94@gmail.com>
2021-03-09 11:36:11 +0100

    Add documentation for the S4 methods

--- R/spatialDE.R
+++ R/spatialDE.R
@@ -1,4 +1,63 @@
-
+#' Find spatially variable genes with **SpatialDE**
+#'
+#' Identify genes that significantly depend on spatial coordinates with the
+#' [**SpatialDE**](https://github.com/Teichlab/SpatialDE) Python package.
+#'
+#' @param x A numeric `matrix` of counts where genes are rows and cells are columns.
+#'
+#'    Alternatively, a \linkS4class{SpatialExperiment} object.
+#'
+#' @param ... For the generic, arguments to pass to specific methods.
+#' @param coordinates A `data.frame` with sample coordinates. Each row is a
+#'   sample, the columns with coordinates should be named 'x' and 'y'.
+#'
+#'   For the *SpatialExperiment* method, coordinates are taken from
+#'   `spatialCoords(x)`.
+#'
+#' @param assay_type A `character` string specifying the assay from `x` to use
+#'   as input. Defaults to `"counts"`.
+#' @param verbose A `logical` controlling the display of a progress bar from the
+#'   Python package.
+#'
+#' @return A `data.frame` with DE results where each row is a gene and columns
+#'   contain relevant statistics.
+#'
+#'   The most important columns are:
+#'
+#'   * `g`: the name of the gene
+#'   * `pval`: the p-value for spatial differential expression
+#'   * `qval`: the q-value, indicating significance after correcting for
+#'   multiple testing
+#'   * `l`: A parameter indicating the distance scale a gene changes expression
+#'   over
+#'
+#' @examples
+#' ## Mock up a SpatialExperiment object wit 100 cells, 200 genes
+#' set.seed(42)
+#' spe <- mockSVG(size = 10, tot_genes = 200, de_genes = 20, return_SPE = TRUE)
+#'
+#' ## Run spatialDE
+#' de_results <- spatialDE(spe)
+#' head(de_results)
+#'
+#' @seealso
+#' The individual steps performed by this function: [stabilize()],
+#' [regress_out()] and [run()].
+#'
+#' For further analysis of the DE results:
+#' [model_search()] and [spatial_patterns()].
+#'
+#' @references
+#' Svensson, V., Teichmann, S. & Stegle, O. SpatialDE: identification of
+#' spatially variable genes. Nat Methods 15, 343–346 (2018).
+#' \url{https://doi.org/10.1038/nmeth.4636}
+#'
+#' [**SpatialDE 1.1.3**](https://pypi.org/project/SpatialDE/1.1.3/): the version
+#' of the Python package used under the hood.
+#'
+#' @author Davide Corso, Milan Malfait
+#' @name spatialDE
+NULL
 
 ## Run spatialDE pipeline on any matrix-like object
 #' @importFrom Matrix colSums
