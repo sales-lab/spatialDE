18009cb6c4d80801008a152103b5006040638e2c
Gabriele Sales <gbrsales@gmail.com>
2023-01-23 18:44:09 +0100

    Pin numpy, patsy and pandas versions

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,15 +1,7 @@
 .spatialDE_dependencies <- c(
-    #"click==8.0.1",
-    #"numpy==1.21.0",
-    "python==3.8.5",
-    "pandas==0.25.3",
-    "patsy==0.5.1",
-    "pip==21.0.1"#,
-    #"python-dateutil==2.8.1",
-    #"scipy==1.7.0",
-    #"six==1.16.0",
-    #"tqdm==4.61.1",
-    #"wheel==0.36.2"
+    "numpy==1.23.5",
+    "patsy==0.5.3",
+    "pandas==1.5.2"
 )
 #' @importFrom basilisk BasiliskEnvironment
 spatialDE_env <- BasiliskEnvironment(
